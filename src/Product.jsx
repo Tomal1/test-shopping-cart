da9c36c727cc477ea2b03864d652404486ecf4ae
MI <minhazulislam@hotmail.com>
2025-04-18 16:08:09 +0100

    styling added to the Product section

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,4 +1,26 @@
+import "./assets/style/product.css"
 
-const Product =() => <p1>hello world</p1>
+
+const Product =() => {
+    return(
+        <>
+            <div id="Con">
+                <div id="imageOfProduct"></div>
+                <div id="quanOfProduct">
+
+                    <span>0</span>
+                </div>
+                <div id="btnOfProduct">
+                    <button>+</button>
+                    <button>-</button>
+                </div>
+                <div id="toBasket">
+                    <button>Basket</button>
+                </div>
+            </div>
+        </>
+    )
+
+}
 
 export default Product
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import Product from './Product.jsx'
+import Product from "./product.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
