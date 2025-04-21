fd3a694322e7f55e4a0541b463c89a5de5cbd8ce
MI <minhazulislam@hotmail.com>
2025-04-21 22:55:37 +0100

    npm i react-router-dom

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,39 +1,31 @@
-import "./assets/style/product.css"
+import "./assets/style/product.css";
+import { useState } from "react";
 
-import { useState } from "react"
+const Product = () => {
+  const [value, setValue] = useState(0);
+  const quan = (ans) => {
+    ans == "+" ? setValue(value + 1) : setValue(value - 1);
+  };
 
+  return (
+    <>
+      <div id="Con">
+        <div id="imageOfProduct"></div>
+        <div id="quanOfProduct">
+          <span>{value}</span>
+        </div>
 
+        <div id="btnOfProduct">
+          <button onClick={() => quan("+")}>+</button>
+          <button onClick={() => quan("")}>-</button>
+        </div>
 
+        <div id="toBasket">
+          <button>Basket</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
-const Product =() => {
-    const [value, setValue ] = useState(0);
-
-    const quan = (ans)=>{
-        ans == "+" ? setValue(value+1) : setValue(value -1); 
-    }
-
-    return(
-        <>
-            <div id="Con">
-                <div id="imageOfProduct"></div>
-                <div id="quanOfProduct">
-
-                    <span>{value}</span>
-                </div>
-
-                <div id="btnOfProduct">
-                    <button onClick={()=>quan("+")}>+</button>
-                    <button onClick={()=>quan("")}>-</button>
-                </div>
-
-
-                <div id="toBasket">
-                    <button>Basket</button>
-                </div>
-            </div>
-        </>
-    )
-
-}
-
-export default Product
+export default Product;
