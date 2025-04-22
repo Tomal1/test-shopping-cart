167ab8652e502727579e17a729cee89e848f0c0d
MI <minhazulislam@hotmail.com>
2025-04-22 20:58:32 +0100

    styling added to the basket page with a dummy item

--- src/basket.jsx
+++ src/basket.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./assets/style/basket.css"
 
 const Basket = () => {
   let navigate = useNavigate();
@@ -12,6 +13,16 @@ const Basket = () => {
     <>
       <h1>This is the Basket page</h1>
       <button onClick={() => handleClick()}>Shopping</button>
+      <div className="basketCon">
+        <div className="itemCon">
+          <h4>name of Item</h4>
+          <button>+</button>
+          <h4><span>quantaty</span></h4>
+          <button>-</button>
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </div>
     </>
   );
 };
